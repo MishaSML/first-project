96457e700ddd9c86309188b306786676353ea73c
MishaSML <smolmish.a@yandex.ru>
2020-09-05 14:27:07 +0300

    add Goodbuy

--- src/main/java/ru/project/firstapp/Main.java
+++ src/main/java/ru/project/firstapp/Main.java
@@ -3,5 +3,6 @@ package ru.project.firstapp;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.print("Goodbuy");
     }
 }
